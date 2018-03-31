74c162c11ee31e9fb5928749a7c0925439818cc6
Shelby Inloes <shelbyinloes@gmail.com>
2018-03-31 10:13:09 -0500

    components & api

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const api_key = "3e7a6ba7218095907d2e8f8882626c5d"
 
 class App extends Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${api_key}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">WeatherAPI</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
       </div>
     );
   }
