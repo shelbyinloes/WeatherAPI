627be5d2e06fb3871cd07f1a728eac4a3ae5038d
Shelby Inloes <shelbyinloes@gmail.com>
2018-04-03 17:14:44 -0500

    no changes

--- weather/src/components/Weather.js
+++ weather/src/components/Weather.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const weatherStyle ={ 
-    backgroundColor: "#FFBCB4",
-    paddingBottom: 20
+    backgroundColor: "#FFBCB4", 
+    padding: 20
 }
 
 const spanStyle ={
@@ -14,18 +14,10 @@ const spanStyle ={
 const Weather = props => (
         <div className="container" style={weatherStyle}>
             {props.city && props.country && <p><span style={spanStyle}>Location:</span> {props.city}, {props.country}</p>}  
-            {
-            props.temp && <p><span style={spanStyle}>Temperature:</span> {props.temp} F</p>
-            }
-            {
-            props.humidity && <p><span style={spanStyle}>Humidity:</span> {props.humidity}%</p>
-            }
-            {
-            props.description && <p><span style={spanStyle}>Conditions:</span> {props.description}</p>
-            }
-            {
-            props.error && <p>{props.error}</p>
-            }
+            {props.temp && <p><span style={spanStyle}>Temperature:</span> {props.temp} F</p>}
+            {props.humidity && <p><span style={spanStyle}>Humidity:</span> {props.humidity}%</p>}
+            {props.description && <p><span style={spanStyle}>Conditions:</span> {props.description}</p>}
+            {props.error && <p>{props.error}</p>}
         </div>
     );
 
