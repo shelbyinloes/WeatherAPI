f2264aff16461d5b88ef70fe779b8f072e202b0a
Shelby Inloes <shelbyinloes@gmail.com>
2018-04-03 18:14:26 -0500

    https

--- weather/src/App.js
+++ weather/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}&units=imperial`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}&units=imperial`);
     const data = await api_call.json();
 
     if (city && country){
